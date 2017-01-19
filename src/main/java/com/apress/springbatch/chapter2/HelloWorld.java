a68e02f7a4f36cb448a1a7274eb4120907780ee2
soulmaru <daniel.mf.souza@gmail.com>
2017-01-18 22:32:25 -0200

    Estudo até Working with Job Listeners

--- src/main/java/com/apress/springbatch/chapter2/HelloWorld.java
+++ src/main/java/com/apress/springbatch/chapter2/HelloWorld.java
@@ -5,11 +5,21 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
 public class HelloWorld implements Tasklet {
-	private static final String HELLO_WORLD = "Hello, world!";
+	private static final String HELLO_WORLD = "Hello, %s!";
+	private String name;
 	
-	public RepeatStatus execute( StepContribution arg0, ChunkContext arg1 ) throws Exception
+	public RepeatStatus execute( StepContribution step, ChunkContext context ) throws Exception
 	{
-		System.out.println( HELLO_WORLD );
+		System.out.println(String.format(HELLO_WORLD,name));
 		return RepeatStatus.FINISHED;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}	
+	
 }
